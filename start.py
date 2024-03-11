21472ef43d7b67c7f1bf6124c33f933d1f7041cf
Exched <143643800+Exched@users.noreply.github.com>
2024-03-12 06:56:12 +0700

    Update start.py

--- start.py
+++ start.py
@@ -27,7 +27,7 @@ class MOONTON:
     def progress_bar(self, current, total, bar_length=40):
         progress = int(current / total * bar_length)
         bar = '=' * progress + '-' * (bar_length - progress)
-        sys.stdout.write('\r[+] Progress: [{0}] {1}/{2} ({3:.2f}%)'.format(bar, current, total, current / total * 100))
+        sys.stdout.write('\r\033[94;3m[+] Progress: [{0}] {1}/{2} ({3:.2f}%)\033[0m'.format(bar, current, total, current / total * 100))
         sys.stdout.flush()
 
     def main(self):
@@ -37,10 +37,10 @@ class MOONTON:
 ██║░░██║██║░░██║██║╚██╔╝██║██╔══╝░░░╚═══██╗██╔══██║██║░░░██║██║╚██╔╝██║██╔══╝░░
 ██████╔╝╚█████╔╝██║░╚═╝░██║███████╗██████╔╝██║░░██║╚██████╔╝██║░╚═╝░██║███████╗
 ╚═════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝░╚═════╝░╚═╝░░░░░╚═╝╚══════╝''')
-        print("\n[+] Mohon tunggu, sedang memeriksa akun...")
+        print("\n\033[92m[+] Mohon tunggu, sedang memeriksa akun...\033[0m")
         time.sleep(3)
         os.system('cls' if os.name == 'nt' else 'clear')
-        empas = input('[?] List empas (ex: list.txt): ')
+        empas = input('\033[92m[?] List empas (ex: list.txt): \033[0m')
         if os.path.exists(empas):
             total_accounts = sum(1 for line in open(empas))
             for data in open(empas, 'r').readlines():
@@ -64,7 +64,7 @@ class MOONTON:
                     except:
                         pass
             if len(self.userdata) == 0:
-                exit('[!] Empas tidak ada atau tidak valid pastikan berformat email:pass atau email|pass')
+                exit('\033[91m[!] Empas tidak ada atau tidak valid pastikan berformat email:pass atau email|pass\033[0m')
             print('\033[92m[*] Total {0} Account\033[0m\n'.format(str(len(self.userdata))))
             for _ in range(3):
                 for i, user in enumerate(self.userdata, 1):
@@ -76,7 +76,7 @@ class MOONTON:
             print('\033[91m[#] GAGAL: {0} - saved: die.txt\033[0m'.format(str(len(self.die))))
             exit(0)
         else:
-            print('[!] File tidak ditemukan "{0}"'.format(empas))
+            print('\033[91m[!] File tidak ditemukan "{0}"\033[0m'.format(empas))
 
     def build_params(self, user):
         md5 = hashlib.new('md5')
